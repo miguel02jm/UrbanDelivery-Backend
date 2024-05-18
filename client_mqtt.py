008cc3f821d101b60c7dcb3e6c8e1808d5f3d3d2
majm0013 <104778314+miguel02jm@users.noreply.github.com>
2024-05-18 17:14:19 +0200

    Some improvements made to the handle_messages method, implementation of a method to receive the robot's odometry.

--- client_mqtt.py
+++ client_mqtt.py
@@ -1,14 +1,15 @@
 from flask_mqtt import Mqtt
 from app import app
 
-app.config['MQTT_BROKER_URL'] = '192.168.1.151'
+app.config['MQTT_BROKER_URL'] = '0.0.0.0'
 app.config['MQTT_BROKER_PORT'] = 1883
 app.config['MQTT_USERNAME'] = ''
 app.config['MQTT_PASSWORD'] = ''
 app.config['MQTT_KEEPALIVE'] = 5
 app.config['MQTT_TLS_ENABLED'] = False
-topic = '/GrupoP/ruta'
+topic = '/GroupP/ruta'
 topic2 = '/map'
+topic3 = '/posRobot'
 
 mqtt_client = Mqtt(app)
 
@@ -18,5 +19,6 @@ def handle_connect(client, userdata, flags, rc):
        print('Connected successfully')
        mqtt_client.subscribe(topic)
        mqtt_client.subscribe(topic2)
+       mqtt_client.subscribe(topic3)
    else:
        print('Bad connection. Code:', rc)
--- routes.py
+++ routes.py
@@ -1,35 +1,53 @@
 from flask import request, jsonify
 from app import app
-from functions import dijkstra, path_to_movement, encodedMap_to_graph
-from variables import rows, cols, mapa_code, mqtt_messages
+from functions import dijkstra, path_to_movement, encodedMap_to_graph, delivery2movement
+from variables import rows, cols, mapa_code, mapa_code, pos_inicial_robot, pos_robot
 from client_mqtt import mqtt_client, topic
 
-@app.route('/InteligenciaAmbiental/recibirMapaCode', methods=['GET'])
-def get_mqtt_messages():
-    global mqtt_messages
-    return jsonify({"message": mqtt_messages.strip()})
+@app.route('/recibirMapaCode', methods=['GET'])
+def get_mapa_code():
+    global mapa_code
+    return jsonify({"mapaCode": mapa_code.strip()})
+
+@app.route("/recibirPosicionRobot", methods=['GET'])
+def get_pos_robot():
+    global pos_robot
+    return jsonify({"posRobot": pos_robot})
 
 @mqtt_client.on_message()
-def handle_and_get_mqtt_message(client, userdata, message):
-    global mqtt_messages
-    mqtt_messages = message.payload.decode('utf-8')
-    with app.app_context():
-        response = get_mqtt_messages()
-    return mqtt_messages
+def handle_messages(client, userdata, message):
+    global mapa_code
+    global pos_robot
 
-@app.route('/InteligenciaAmbiental/enviarSalidaLlegada', methods=['POST'])
+    response = None
+
+    if message.topic == '/map':
+        mapa_code = message.payload.decode('utf-8')
+        with app.app_context():
+            response = get_mapa_code()
+    elif message.topic == '/posRobot':
+        pos_robot = message.payload.decode('utf-8')
+        with app.app_context():
+            response = get_pos_robot()
+
+    if response is not None:
+        return response
+    else:
+        with app.app_context():
+            return jsonify({'error': 'No response'})
+
+@app.route('/enviarSalidaLlegada', methods=['POST'])
 def publish_message():
-    global resultado
+    global resultado, pos_inicial_robot
     resultado = encodedMap_to_graph(rows, cols, mapa_code)
     if request.method == 'POST':
-        request_data = request.get_json()
         if resultado is None:
             return jsonify({'mensaje': 'Aún no se ha procesado el mapa'})
         data = request.get_json()
         ini = int(data['salida'])
         fin = int(data['llegada'])
-        path = dijkstra(resultado, ini, fin)
-        movement = path_to_movement(path, cols=cols, rows=rows)
+        movement = delivery2movement(ini, fin, pos_inicial_robot, resultado)
+        pos_inicial_robot = fin
         movements_str = ','.join(movement)
         publish_result = mqtt_client.publish(topic, movements_str)
         return jsonify({'code': publish_result[0]})
